778f0ef8bcf0931390a6a0dde293bb94e08ff493
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2014-09-13 17:58:08 +0200

    velocity

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ for (i=0;i<elements.length;i++) {
         elements[i].removeAttribute('data-svg');
     }
 }
+
+$("#hello").velocity("transition.slideDownBigIn",
+                     { duration: 1500 });
+$("#planete-anneaux").velocity("transition.slideDownBigIn",
+                     { duration: 1500 });
+$("#planete-rouge").velocity("transition.expandIn",
+                             { duration: 1500 });
+
+$("#aurelien-sur-la-lune").velocity("transition.bounceLeftIn",
+                                    {duration: 1500});
+$("#planete-rayures").velocity("transition.bounceLeftIn",
+                               {duration: 1500});
+$("#planete-vie").velocity("transition.bounceRightIn",
+                               {duration: 1500});
