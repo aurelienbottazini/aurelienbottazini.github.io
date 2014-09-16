2a73782390b0deb4afcc82c08292976e88f177d1
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2014-09-16 14:57:44 +0200

    saving before hello i am aurelien

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@ $("#planete-vie").velocity("transition.bounceRightIn",
 $('#planete-rouge-circles circle:eq(0)').
     velocity({ fill: "#ff694d"}, {duration: 2000, loop: true,
                                   delay: 600});
-
 $('#planete-rouge-circles circle:eq(1)'). velocity({ fill:
                                                      "#ff694d"}, {duration: 2000, loop: true, delay: 600*2});
 
@@ -37,7 +36,6 @@ $('#planete-rouge-circles circle:eq(3)'). velocity({ fill:
 
 $('#planete-rouge-circles circle:eq(4)'). velocity({ fill:
                                                      "#ff694d"}, {duration: 2000, loop: true, delay: 600*5});
-
 
 $('#planete-rouge-circles path:eq(0)').
     velocity({ fill: "#ff694d"},
@@ -63,3 +61,50 @@ $('#planete-rouge-circles path:eq(6)').
 $('#planete-rouge-circles path:eq(7)').
     velocity({ fill: "#ff694d"},
              {delay: 4500, duration: 2000, loop: true });
+
+
+(function() {
+  window.signature = {
+    initialize: function() {
+      return $('#hello-aurelien').each(function() {
+        var delay, length, path, paths, previousStrokeLength, speed, _i, _len, _results;
+        paths = $('path, circle, rect', this);
+        delay = 0;
+        _results = [];
+        for (_i = 0, _len = paths.length; _i < _len; _i++) {
+          path = paths[_i];
+          length = path.getTotalLength();
+          previousStrokeLength = speed || 0;
+          speed = length < 100 ? 20 : Math.floor(length);
+          delay += previousStrokeLength + 100;
+          _results.push($(path).css('transition', 'none').attr('data-length', length).attr('data-speed', speed).attr('data-delay', delay).attr('stroke-dashoffset', length).attr('stroke-dasharray', length + ',' + length));
+        }
+        return _results;
+      });
+    },
+    animate: function() {
+      return $('#hello-aurelien').each(function() {
+        var delay, length, path, paths, speed, _i, _len, _results;
+        paths = $('path, circle, rect', this);
+        _results = [];
+        for (_i = 0, _len = paths.length; _i < _len; _i++) {
+          path = paths[_i];
+          length = $(path).attr('data-length');
+          speed = $(path).attr('data-speed');
+          delay = $(path).attr('data-delay');
+          _results.push($(path).css('transition', 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear').attr('stroke-dashoffset', '0'));
+        }
+        return _results;
+      });
+    }
+  };
+
+  $(document).ready(function() {
+    window.signature.initialize();
+  });
+
+  $(window).load(function() {
+    return window.signature.animate();
+  });
+
+}).call(this);
