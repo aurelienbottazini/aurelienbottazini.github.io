92a87bfde9603e11bb55404e67a30911b3398259
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2014-09-16 11:20:49 +0200

    planete-rouge animation

--- js/main.js
+++ js/main.js
@@ -22,8 +22,44 @@ $("#planete-rayures").velocity("transition.bounceLeftIn",
 $("#planete-vie").velocity("transition.bounceRightIn",
                            {duration: 1500, delay: 500});
 
-$('#planete-rouge-circles circle').velocity({ fill: "#ff694d"},
-                                            {duration: 3000, loop:
-                                             true });
-$('#planete-rouge-circles path').velocity({ fill: "#ff694d"},
-                                            {duration: 3000, loop: true });
+$('#planete-rouge-circles circle:eq(0)').
+    velocity({ fill: "#ff694d"}, {duration: 2000, loop: true,
+                                  delay: 600});
+
+$('#planete-rouge-circles circle:eq(1)'). velocity({ fill:
+                                                     "#ff694d"}, {duration: 2000, loop: true, delay: 600*2});
+
+$('#planete-rouge-circles circle:eq(2)'). velocity({ fill:
+                                                     "#ff694d"}, {duration: 2000, loop: true, delay: 600*3});
+
+$('#planete-rouge-circles circle:eq(3)'). velocity({ fill:
+                                                     "#ff694d"}, {duration: 2000, loop: true, delay: 600*4});
+
+$('#planete-rouge-circles circle:eq(4)'). velocity({ fill:
+                                                     "#ff694d"}, {duration: 2000, loop: true, delay: 600*5});
+
+
+$('#planete-rouge-circles path:eq(0)').
+    velocity({ fill: "#ff694d"},
+             {delay: 1000, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(1)').
+    velocity({ fill: "#ff694d"},
+             {delay: 900, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(2)').
+    velocity({ fill: "#ff694d"},
+             {delay: 1500, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(3)').
+    velocity({ fill: "#ff694d"},
+             {delay: 2000, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(4)').
+    velocity({ fill: "#ff694d"},
+             {delay: 3000, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(5)').
+    velocity({ fill: "#ff694d"},
+             {delay: 2500, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(6)').
+    velocity({ fill: "#ff694d"},
+             {delay: 4000, duration: 2000, loop: true });
+$('#planete-rouge-circles path:eq(7)').
+    velocity({ fill: "#ff694d"},
+             {delay: 4500, duration: 2000, loop: true });
