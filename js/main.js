2811496aa2c26d0497e6dc556361f4fef0337749
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2014-09-25 15:14:50 +0200

    contact svg effect

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 var i, src, extension = Modernizr.svg ? '.svg' : '.png';
 var planete_rouge = $('#planete-rouge');
 var planete_vie = $('#planete-vie');
-
 var planete_rouge_circles = $('#planete-rouge-circles');
 var planete_rouge_circles_circles =
         planete_rouge_circles.find('circle');
 var circles_number = planete_rouge_circles_circles.length;
 var planete_rouge_paths = planete_rouge_circles.find('path');
-var paths_number = planete_rouge_paths.length;
+var planete_rouge_paths_number = planete_rouge_paths.length;
 var planete_rouge_about_me_letters =
         $('#planete-rouge-about-me-letters').find('path');
 var planete_rouge_about_me_letters_number =
@@ -22,21 +21,28 @@ for (i=0;i<elements.length;i++) {
     }
 }
 
-var hello_paths = $('#hello-i-am-aurelien path');
-var hello_paths_length = hello_paths.length;
-var svg_path;
-var svg_path_length;
-for(i = 0; i< hello_paths_length; i++) {
+var draw_text = function(paths_selector) {
+    var paths = $(paths_selector);
+    var paths_length = paths.length;
+    var svg_path;
+    var svg_path_length;
+    for(i = 0; i< paths_length; i++) {
 
-    svg_path = hello_paths.eq(i);
-    svg_path_length = svg_path[0].getTotalLength();
+        svg_path = paths.eq(i);
+        svg_path_length = svg_path[0].getTotalLength();
 
-    svg_path.css({"fill-opacity": 0,
-                  "stroke-dasharray": svg_path_length,
-                  "stroke-dashoffset": svg_path_length})
-        .velocity({strokeDashoffset: 0}, {duration: 3000, delay: 600})
-        .velocity({fillOpacity: [1, 0], strokeOpacity:[0,1]});
-}
+        svg_path.css({"fill-opacity": 0,
+                      "stroke-dasharray": svg_path_length,
+                      "stroke-dashoffset": svg_path_length})
+            .velocity({strokeDashoffset: 0}, {duration: 3000, delay: 600})
+            .velocity({fillOpacity: [1, 0], strokeOpacity:[0,1]});
+    }
+
+};
+draw_text('#hello-i-am-aurelien path');
+draw_text('#planete-rouge-about-me-letters path');
+draw_text('#planete-vie-contact-letters path');
+
 
 $("#planete-anneaux").velocity("transition.slideDownBigIn",
                                { duration: 1500, delay: 500 });
@@ -49,10 +55,46 @@ $("#planete-rayures").velocity("transition.bounceLeftIn",
 $("#planete-vie").velocity("transition.bounceRightIn",
                            {duration: 1500, delay: 500});
 
+var planete_vie_contact_letters =
+        $('#planete-vie-contact-letters path');
+var planete_vie_contact_letters_number =
+        planete_vie_contact_letters.length;
+var planete_vie_continents = $('#planete-vie-continents path');
+var planete_vie_continents_number = planete_vie_continents.length;
 planete_vie.mouseenter(function() {
-    planete_vie.velocity({scaleX: 1.3, scaleY: 1.3}, {duration: 500});
+    planete_vie.velocity({scaleX: 1.3, scaleY: 1.3}, {duration:
+                                                      500});
+
+    for(i=0 ;i < planete_vie_contact_letters_number; i++) {
+        planete_vie_contact_letters
+            .eq(i)
+            .velocity({ fill: "#11ea96"},
+                      {duration: 500});
+    }
+
+    for(i=0 ;i < planete_vie_continents_number; i++) {
+        planete_vie_continents
+            .eq(i)
+            .velocity({ fill: "#11ea96"},
+                      {duration: 500});
+    }
+
 }).mouseleave(function() {
     planete_vie.velocity({scaleX: 1, scaleY: 1}, {duration: 500});
+
+    for(i=0 ;i < planete_vie_contact_letters_number; i++) {
+        planete_vie_contact_letters
+            .eq(i)
+            .velocity({ fill: "#1b8e65"},
+                      {duration: 500});
+    }
+
+    for(i=0 ;i < planete_vie_continents_number; i++) {
+        planete_vie_continents
+            .eq(i)
+            .velocity({ fill: "#1b8e65"},
+                      {duration: 500});
+    }
 });
 
 planete_rouge.mouseenter(function() {
@@ -65,7 +107,7 @@ planete_rouge.mouseenter(function() {
             .velocity({ fill: "#ff694d"},
                       {duration: 500});
     }
-    for(i=0; i < paths_number; i++) {
+    for(i=0; i < planete_rouge_paths_number; i++) {
         planete_rouge_paths.eq(i).
             velocity({ fill: "#ff694d"},
                      {duration: 500});
@@ -87,7 +129,7 @@ planete_rouge.mouseenter(function() {
             .velocity({ fill: "#c22327"},
                       {duration: 500});
     }
-    for(i=0; i < paths_number; i++) {
+    for(i=0; i < planete_rouge_paths_number; i++) {
         planete_rouge_paths.eq(i).
             velocity({ fill: "#c22327"},
                      {duration: 500});
@@ -106,4 +148,27 @@ $('#about-link').click(function() {
 $('#contact-link').click(function() {
     $('#contact').velocity("scroll", 1000);
     return false;
+});
+
+var animate_contact_points = function () {
+}
+
+var contact_points = $('#contact-points path');
+var contact_points_length = contact_points.length;
+$('#contact').mouseenter(function(){
+    for(i=contact_points_length; i >= 0; i--) {
+        contact_points.eq(i).
+            velocity({ fill: "#22252e"},
+                     {duration: 50,
+                      delay: 500+ 50*(contact_points_length-i)
+                     });
+    }
+}).mouseleave(function(){
+
+    for(i=contact_points_length; i >= 0; i--) {
+        contact_points.eq(i).velocity('stop');
+        contact_points.eq(i).
+            velocity({ fill: "#adf4e4"});
+    }
+
 });
