f13136d9b0f039e1752b6f0e4ad32736fd01d94f
Aurélien Bottazzini <aurelien@bottazini.com>
2016-10-18 13:49:19 +0200

    cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,114 +1,87 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        bower_concat: {
-            all: {
-                dest: '_js/_bower.js',
-                cssDest: '_css/_bower.css',
-                exclude: [
-                  'velocity',
-                ],
-                dependencies: {
-                },
-                bowerOptions: {
-                    relative: false
-                },
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower_concat: {
+      all: {
+        dest: '_js/_bower.js',
+        cssDest: '_css/_bower.css',
+        exclude: [
+          'velocity',
+        ],
+        dependencies: {
         },
-        uglify: {
-            bower: {
-              options: {
-                mangle: true,
-                compress: {}
-              },
-              files: {
-                'js/home.min.js':
-                [
-                  '_js/_bower.js',
-                  '_js/common.js',
-                  '_js/home.js'],
-                'js/resume.min.js':
-                ['_js/_bower.js',
-                 '_js/common.js'],
-                'js/default.min.js':
-                [
-                  '_js/_bower.js',
-                  '_js/common.js',
-                ],
-              }
-            },
+        bowerOptions: {
+          relative: false
         },
-        sass: {                              // Task
-            dist: {                            // Target
-                options: {                       // Target options
-                    style: 'compressed'
-                },
-                files: {
-                    '_css/default.css': '_sass/default.scss',
-                    '_css/home.css': '_sass/home.scss',
-                    'css/resume.css': '_sass/resume.scss',
-                    'easyLearning/css/main.css': '_sass/easylearning.scss',
-                    'cubex/css/main.css': '_sass/cubex.scss',
-                    '_css/htmlize.css': '_sass/htmlize.scss'
-                }
-            }
+      }
+    },
+    uglify: {
+      bower: {
+        options: {
+          mangle: false,
+          compress: false
         },
-        cssmin: {
-            combine: {
-                files: {
-                    'css/default.min.css': ['_css/_bower.css', '_css/default.css'],
-                    'css/resume.css': ['_css/_bower.css', 'css/resume.css'],
-                    'css/home.min.css': ['_css/_bower.css', '_css/home.css'],
-                    'easyLearning/css/main.css': ['_css/_bower.css', 'easyLearning/css/main.css'],
-                    'cubex/css/main.css': ['_css/_bower.css', 'cubex/css/main.css'],
-                }
-            },
-        },
-
-        watch: {
-            scripts: {
-                files: ['_js/*.js'],
-                tasks: ['uglify', 'exec:jekyll'],
-                options: {
-                    spawn: false,
-                    interrupt: true
-                },
-            },
-            css: {
-                files: ['_sass/*.scss'],
-                tasks: ['sass', 'cssmin', 'exec:jekyll'],
-                options: {
-                    spawn: false,
-                    interrupt: true
-                }
-            },
-            anything_but_underscore: {
-                files: ['**/*.html', '**/*.[gif|svg|jpg]', '!_*/**'],
-                tasks: ['exec:jekyll'],
-                options: {
-                    spawn: false,
-                    interrupt: true
-                }
-            }
-        },
-
-        exec: {
-            jekyll: {
-                cmd: 'bundle exec jekyll build'
-            },
-            create_sketchnotes_yaml: {
-                cmd: "ruby -e \"require 'yaml';File.open('_data/sketchnotes.yml', 'w') { |f| f.write Dir.new('sketchnotes').entries.select { |e| e =~ /\.jpg/}.to_yaml }\""
-            }
+        files: {
+          'js/home.min.js':
+          [
+            '_js/_bower.js',
+            '_js/common.js',
+            '_js/home.js'],
+          'js/resume.min.js':
+          ['_js/_bower.js',
+           '_js/common.js'],
+          'js/default.min.js':
+          [
+            '_js/_bower.js',
+            '_js/common.js',
+          ],
         }
-    });
-    grunt.loadNpmTasks('grunt-bower-concat');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-exec');
+      },
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'css/default.min.css': ['_css/_bower.css', '_css/default.css'],
+          'css/resume.css': ['_css/_bower.css', 'css/resume.css'],
+          'css/home.min.css': ['_css/_bower.css', '_css/home.css'],
+          'easyLearning/css/main.css': ['_css/_bower.css', 'easyLearning/css/main.css'],
+          'cubex/css/main.css': ['_css/_bower.css', 'cubex/css/main.css'],
+        }
+      },
+    },
 
-    grunt.registerTask('default', ['prepare', 'exec:jekyll']);
-    grunt.registerTask('prepare', ['bower_concat', 'sass', 'cssmin', 'uglify', 'exec:create_sketchnotes_yaml']);
+    watch: {
+      scripts: {
+        files: ['_js/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+          interrupt: true
+        },
+      },
+      css: {
+        files: ['_css/*.css'],
+        tasks: ['cssmin'],
+        options: {
+          spawn: false,
+          interrupt: true
+        }
+      },
+    },
+
+    exec: {
+      create_sketchnotes_yaml: {
+        cmd: "ruby -e \"require 'yaml';File.open('_data/sketchnotes.yml', 'w') { |f| f.write Dir.new('sketchnotes').entries.select { |e| e =~ /\.jpg/}.to_yaml }\""
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('default', ['prepare']);
+  grunt.registerTask('prepare', ['bower_concat', 'cssmin', 'uglify', 'exec:create_sketchnotes_yaml']);
 };
--- _js/common.js
+++ _js/common.js
@@ -23,3 +23,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   }
 });
+
+var isAnimating = false;
+
+$('#main_navigation').on('click', '[data-type="page-transition"]', function(event){
+  event.preventDefault();
+  //detect which page has been selected
+  var newPage = $(this).attr('href');
+
+
+  var links = document.querySelectorAll("#main_navigation ul li a");
+  var pathname = location.href;
+
+  for(var i = 0, length = links.length; i < length; i += 1) {
+    links[i].classList.remove('current');
+  }
+
+  this.classList.add('current');
+  //if the page is not animating - trigger animation
+  if( !isAnimating ) changePage(newPage, true);
+});
+
+function changePage(url, bool) {
+  isAnimating = true;
+  $('body').addClass('page-is-changing');
+  loadNewContent(url, bool);
+}
+
+function loadNewContent(url, bool) {
+  var newSectionName = 'cd-'+url.replace('.html', ''),
+  section = $('<div id="wrapper" class="cd-main-content '+newSectionName+'"></div>');
+
+  section.load(url + ' #wrapper', function(event){
+    // load new content and replace <main> content with the new one
+    setTimeout(function() {
+      $('#wrapper').html(section);
+      isAnimating = false;
+      $('body').removeClass('page-is-changing');
+      if(url != window.location){
+        window.history.pushState({path: url},'',url);
+      }
+    }, 200);
+  });
+}
+
+$(window).on('popstate', function() {
+    var newPageArray = location.pathname.split('/'),
+        //this is the url of the page to be loaded
+        newPage = newPageArray[newPageArray.length - 1];
+    if( !isAnimating ) changePage(newPage);
+});
