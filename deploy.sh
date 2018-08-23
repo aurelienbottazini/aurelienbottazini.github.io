a3e8042b8b3cf54d02febf57fb536d4ebfdd809b
Aurélien Bottazini <aurelien@bottazini.com>
2018-08-23 14:15:37 +0200

    Automate java version change with deploy.sh
    Necessary because of an incompatibility with s3_website and java >= 10

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ grunt
 
 # s3_website is not compatible with java10 so we need to briefly
 # switch to a previous version while using this script
-sudo update-alternatives --config java
+echo '2' | sudo update-alternatives --config java
 bundle exec s3_website push
-sudo update-alternatives --config java
+echo '0' | sudo update-alternatives --config java
