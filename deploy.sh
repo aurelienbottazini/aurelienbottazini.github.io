21273e147f2e4611075a4e63d33d18eed4904f5c
Aurélien Bottazini <aurelien@bottazini.com>
2019-01-20 16:49:20 +0100

    change deploy script for blog

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 grunt
 jekyll build
 
-# s3_website is not compatible with java10 so we need to briefly
-# switch to a previous version while using this script
-echo '2' | sudo update-alternatives --config java
-bundle exec s3_website push
-echo '0' | sudo update-alternatives --config java
+cd ~/projects/aurelienbottazini.com_public
+git add .
+git commit -m "publish"
+g push
+cd -
