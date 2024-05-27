8dc3a6e509578559444f0f471df553f34aa7722f
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2024-05-27 16:57:54 +0200

    do not cache chrome extensions of pixel tracker

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = "aurelienbottazini.com-v6";
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   function addDefaultUrlsToCache() {
     const urlsToCache = [
       "/offline.html",
       "/notes.html",
       "/about.html",
       "/resume.html",
-      "https://fonts.googleapis.com/css?family=Alegreya:400,400i|Passion+One|Permanent+Marker"
+      "https://fonts.googleapis.com/css?family=Alegreya:400,400i|Passion+One|Permanent+Marker",
     ];
-    return caches.open(CACHE_NAME).then(function(cache) {
+    return caches.open(CACHE_NAME).then(function (cache) {
       console.info("Precaching:");
       console.table(urlsToCache);
       return cache.addAll(urlsToCache);
@@ -19,42 +19,48 @@ self.addEventListener("install", function(event) {
   event.waitUntil(addDefaultUrlsToCache());
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
   function deleteOldCaches() {
-    return caches.keys().then(function(cacheNames) {
+    return caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (
             CACHE_NAME !== cacheName &&
             cacheName.startsWith("aurelienbottazini.com-")
           ) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
     });
   }
   event.waitUntil(deleteOldCaches());
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   function isHtmlRequest(event) {
     return event.request.url.endsWith(".html");
   }
   function saveInCache(response) {
-    caches.open(CACHE_NAME).then(cache => cache.add(response.url));
+    if (response.url.startWith("chrome-extension://")) {
+      return;
+    }
+    if (response.url.includes("/fgw")) {
+      return;
+    }
+    caches.open(CACHE_NAME).then((cache) => cache.add(response.url));
   }
 
   event.respondWith(
     fetch(event.request)
-      .then(function(response) {
+      .then(function (response) {
         saveInCache(response);
         return response;
       })
-      .catch(_fetch_error => {
-        return caches.match(event.request).then(function(response) {
+      .catch((_fetch_error) => {
+        return caches.match(event.request).then(function (response) {
           return response || caches.match("/offline.html");
         });
-      })
+      }),
   );
 });
